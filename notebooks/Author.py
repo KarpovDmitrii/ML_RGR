12d5e099a02735553547477bd209b1624067489e
Dmitriy <n_eco@mail.ru>
2024-01-08 16:31:26 +0600

    fd

---
+++ notebooks/Author.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("Разработка Web-приложения (дашборда) для инференса (вывода) моделей ML и анализа данных")
+
+
+st.header("Автор")
+st.write("ФИО: Карпов Дмитрий Александрович")
+st.write("Группа: ФИТ-221")
+st.write("2023 год")
+st.image("./data/My_face.jpg",width =500)
